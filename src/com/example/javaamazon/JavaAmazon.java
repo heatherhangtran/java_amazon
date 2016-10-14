e25b611031831accd552c771c13388ca581c55ef
Heather Hang Tran <heatherhttran@gmail.com>
2016-10-13 19:32:12 -0700

    Testing Jenkins.

---
+++ src/com/example/javaamazon/JavaAmazon.java
@@ -0,0 +1,10 @@
+package com.example.javaamazon;
+
+/**
+ * Created by heatherhangtran on 10/13/16.
+ */
+public class JavaAmazon {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java.");
+    }
+}
